c8aff67235ff9cd825b4f91d85cc3e52f53c3708
Obie <obiemunozjr@gmail.com>
2021-11-18 16:20:26 -0500

    rm unused api comments

--- src/components/API.js
+++ src/components/API.js
@@ -1,34 +1,3 @@
-// API Documentation:
-// https://github.com/harvardartmuseums/api-docs
-//
-// Request Structure:
-// https://api.harvardartmuseums.org/RESOURCE_TYPE?apikey=YOUR_API_KEY
-// fetch(`${BASE_URL}/object?size=1&yearmade=2002&hasimage=1&apikey=${API_KEY}`)
-//
-// AVAILABLE RESOURCES:
-// Object
-// Person
-// Exhibition
-// Publication
-// Gallery
-// Spectrum
-// Classification
-// Century
-// Color
-// Culture
-// Group
-// Medium
-// Period
-// Place
-// Technique
-// Worktype
-// Activity
-// Site
-// Video
-// Image
-// Audio
-// Annotation
-
 const API_KEY = "89ce3864-c616-46cd-975c-c1ddb256d505";
 const BASE_URL = "https://api.harvardartmuseums.org"
 
