729e887b46b11d2e97125b4c6130a39cb3274a9c
Obie <obiemunozjr@gmail.com>
2021-11-18 22:17:10 -0500

    minor updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import hamExplorer from '../images/ham-explorer.png';
 import DatabaseDisplay from './DatabaseDisplay';
 import AboutButton from "./AboutButton";
