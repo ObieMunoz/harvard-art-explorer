6a3ac8cc4d5cebe93cd180af55f7195863c5f684
Obie Munoz <obiemunozjr@gmail.com>
2021-12-11 22:07:27 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function About() {
         <div>
             <div className="ui raised segment" style={{ width: "50%", marginLeft: "auto", marginRight: "auto" }}>
                 <h1 className="ui header">About</h1>
-                <p>This application was developed by <strong>Obie Munoz</strong>, <strong>Chris Valle</strong>, and <strong>Terence Stephens</strong> during the full-time Software Engineering course at the Flatiron School. The application was created to help users explore the Harvard Museum of Art and remotely view objects in their collection.</p>
+                <p>This application was developed during the full-time Software Engineering course at the Flatiron School. The application was created to help users explore the Harvard Museum of Art and remotely view objects in their collection.</p>
                 <p>The application was created with the following technologies:</p>
                 <div className="ui horizontal list">
                     <div className="item">
